0b27e1200606c1633179fedf3aed69ee2eded1b6
Nancy836 <55183861+Nancy836@users.noreply.github.com>
2020-05-16 01:18:14 +0200

    Update login.php

--- flow/login.php
+++ flow/login.php
@@ -5,7 +5,7 @@ if (isset($_POST['login-btn'])){
 
     $username=$_POST['username'];
     $password=$_POST['password'];
-    checkuser($username,$password);
+    CheckUser($username,$password);
 }
 ?>
 
